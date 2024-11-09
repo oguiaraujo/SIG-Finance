7cb2edd37fd0d9e8f9b4cde7593306043a82508b
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2024-11-08 23:49:07 -0300

    Cria Função remove_enter

--- funcoes/funcoes.c
+++ funcoes/funcoes.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+/////////////////
+// Usos Gerais //
+/////////////////
+
+// Função que remove o "\n" após input do usuário
+// Adaptado do ChatGPT
+void remove_enter(char *str){
+	str[strcspn(str, "\n")] = 0;
+}
+
+////////////////
+// Validações //
+////////////////
+
 /* funçao de validação de digito*/
 int checar_digito(int c){
 	  if (c >= '0' && c <= '9') {
--- funcoes/funcoes.h
+++ funcoes/funcoes.h
@@ -1,3 +1,4 @@
 /* Assinaturas das funções */
 
 int checar_digito(char);
+void remove_enter(char *str);
