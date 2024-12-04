53dd1176c208a25efc8586fe964c2dd4f5e11952
Tomé Galileu Oliveira Arcanjo <tome.arcanjo.112@ufrn.edu.br>
2024-12-04 10:56:59 -0300

    Inicia struct do menu despesas

--- despesas/despesas.h
+++ despesas/despesas.h
@@ -1,3 +1,12 @@
+typedef struct despesas Despesas;
+
+struct despesas {
+    char descricao[200];
+    char valor[10];
+    char data[12];
+    char id[5];
+
+};
 
 void menu_despesas(void);
 void cadastrar_despesa(void);
