836101767edd039d91385d106fcedeede963a218
Tomé Galileu Oliveira Arcanjo <tome.arcanjo.112@ufrn.edu.br>
2024-12-16 10:50:42 -0300

    Muda assinatura do cadastrar_tarefas

--- tarefas/tarefas.h
+++ tarefas/tarefas.h
@@ -5,10 +5,11 @@ struct tarefas{
     char descricao[100];
     char prazo[20];
     char id[5];
+    char status;
 };
 
 void menu_tarefas(void);
-void cadastrar_tarefa(Tarefas *tarefa);
+Tarefas* cadastrar_tarefa(void);
 void pesquisar_tarefa(Tarefas *tarefa);
 void atualizar_tarefa(Tarefas *tarefa);
 void excluir_tarefa(Tarefas *tarefa);
